37c0b6c068aa6b5f05095b5e5f3d009eaca35b71
MerabTserodze <augenblick-Reinigung@outlook.com>
2025-06-07 20:03:53 +0200

    Update types.ts

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,1 +1,12 @@
+export interface Room {
+  room_type: 'офис' | 'туалет' | 'склад' | 'кухня';
+  area: number;
+  complexity: 'низкая' | 'средняя' | 'высокая' | 'экстремальная';
+}
+
+export interface EstimateResult {
+  total_price: number;
+  total_time_min: number;
+  estimated_cost: number;
+}
 
